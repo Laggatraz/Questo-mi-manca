58edf0301d20695052b3d9592b3739fb1c985984
Laggatraz <brunofederico431@gmail.com>
2024-10-05 17:46:22 +0200

    ho iniziato ad usare tailwind

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -1,1 +1,61 @@
-<div class="h-20 bg-black"></div>
+<script>
+	import Page from '../routes/+page.svelte';
+</script>
+
+<nav class="bg-[#3c6066] h-14">
+	<div class="flex justify-between items-center">
+		<h1 class="text-2xl font-bold">Questo mi manca!</h1>
+		<div class="">
+			<a class="hover:text-red-500" href="/">Home</a>
+			<a class="hover:text-red-500" href="/collezione">Collezione</a>
+			<a class="hover:text-red-500 hover:bg-red-400" href="/about">About</a>
+		</div>
+	</div>
+</nav>
+
+<!-- Cose da sistemare:
+ distanziare gli elementi dal bordo
+ centrarli verticalmente
+ trovare una palette colori migliore
+ dare spazio agli elementi del menù (padding)
+ distanziare gli elementi del menù tra di loro (border)
+-->
+
+<!-- <style>
+	nav {
+		background-color: rgb(0, 0, 0);
+		color: white;
+		padding: 10px;
+	}
+	nav a:hover {
+		width: 100%;
+		background-color: white;
+		color: black;
+		padding: 2px;
+		transition: 0.5s;
+	}
+
+	.container {
+		display: flex;
+	}
+	ul {
+		display: flex;
+		margin: 0;
+		margin-left: auto;
+		list-style: none;
+	}
+	li {
+		margin-right: 20px;
+		margin-top: 5px;
+	}
+	h1 {
+		margin: 0;
+		font-size: x-large;
+		font: bolder;
+	}
+	a {
+		text-decoration: none;
+		color: white;
+	}
+</style>
+ -->
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,55 +1,7 @@
-<nav>
-	<div class="container">
-		<h1>Questo mi manca!</h1>
-		<ul>
-			<li>
-				<a href="/">Home</a>
-			</li>
-			<li>
-				<a href="/Collezione">Collezione</a>
-			</li>
-			<li>
-				<a href="/Librearia">Libreria</a>
-			</li>
-		</ul>
-	</div>
-</nav>
-<slot />
+<script>
+	import Navbar from '$lib/navbar.svelte';
+	import '../app.css';
+</script>
 
-<style>
-	nav {
-		background-color: rgb(0, 0, 0);
-		color: white;
-		padding: 10px;
-	}
-	nav a:hover {
-		width: 100%;
-		background-color: white;
-		color: black;
-		padding: 2px;
-		transition: 0.5s;
-	}
-
-	.container {
-		display: flex;
-	}
-	ul {
-		display: flex;
-		margin: 0;
-		margin-left: auto;
-		list-style: none;
-	}
-	li {
-		margin-right: 20px;
-		margin-top: 5px;
-	}
-	h1 {
-		margin: 0;
-		font-size: x-large;
-		font: bolder;
-	}
-	a {
-		text-decoration: none;
-		color: white;
-	}
-</style>
+<Navbar />
+<slot></slot>
