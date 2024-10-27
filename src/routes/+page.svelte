80ae4ad8ef494720e8b2b76bf0398c5a159041dc
Laggatraz <brunofederico431@gmail.com>
2024-10-27 15:57:38 +0100

    creato la griglia e aggiunto gli elementi "copertina manga"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
-<h1>Benvenut#!</h1>
+<div class="grid grid-cols-4 gap-4">
+	<div class="m-32 border-2 border-black bg-slate-500 bg-auto pb-60 rounded-t-lg"></div>
+	<div class="m-32 border-2 border-black bg-slate-500 bg-auto pb-60 rounded-t-lg"></div>
+	<div class="m-32 border-2 border-black bg-slate-500 bg-auto pb-60 rounded-t-lg"></div>
+	<div class="m-32 border-2 border-black bg-slate-500 bg-auto pb-60 rounded-t-lg"></div>
+	<div class="m-32 border-2 border-black bg-slate-500 bg-auto pb-60 rounded-t-lg"></div>
+	<div class="m-32 border-2 border-black bg-slate-500 bg-auto pb-60 rounded-t-lg"></div>
+	<div class="m-32 border-2 border-black bg-slate-500 bg-auto pb-60 rounded-t-lg"></div>
+	<div class="m-32 border-2 border-black bg-slate-500 bg-auto pb-60 rounded-t-lg"></div>
+</div>
