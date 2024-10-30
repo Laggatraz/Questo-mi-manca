c9f91ccf137232b49d3cab1a43c66de5a5c70435
Laggatraz <brunofederico431@gmail.com>
2024-10-30 15:44:51 +0100

    grazie beyonce

---
+++ src/lib/CardManga.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export var titolo: string = 'One Piece';
+	export var autore: string = 'Eiichiro Oda';
+	export var prezzo: string = '4,30';
+	export var data: string = '22/07/1997';
+</script>
+
+<main class="pb-1">
+	<div class="h-72 w-64 border-2 border-black rounded-lg flex flex-col overflow-hidden">
+		<div class="bg-orange-500 h-[60%] border-b-2 border-black"></div>
+		<div class="px-2 pt-1 flex flex-col justify-between h-[40%]">
+			<div>
+				<p class="text-2xl italic">{titolo}</p>
+				<p>{autore}</p>
+			</div>
+			<div class="flex justify-between">
+				<p>{data}</p>
+				<p>€{prezzo}</p>
+			</div>
+		</div>
+	</div>
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,26 @@
-<div class="grid grid-cols-4 gap-4">
-	<div class="m-32 border-2 border-black bg-slate-500 bg-auto pb-60 rounded-t-lg"></div>
-	<div class="m-32 border-2 border-black bg-slate-500 bg-auto pb-60 rounded-t-lg"></div>
-	<div class="m-32 border-2 border-black bg-slate-500 bg-auto pb-60 rounded-t-lg"></div>
-	<div class="m-32 border-2 border-black bg-slate-500 bg-auto pb-60 rounded-t-lg"></div>
-	<div class="m-32 border-2 border-black bg-slate-500 bg-auto pb-60 rounded-t-lg"></div>
-	<div class="m-32 border-2 border-black bg-slate-500 bg-auto pb-60 rounded-t-lg"></div>
-	<div class="m-32 border-2 border-black bg-slate-500 bg-auto pb-60 rounded-t-lg"></div>
-	<div class="m-32 border-2 border-black bg-slate-500 bg-auto pb-60 rounded-t-lg"></div>
+<script lang="ts">
+	import CardManga from '$lib/CardManga.svelte';
+	var mangas = [
+		{ titolo: 'One Piece', autore: 'Eiichiro Oda', data: '22/07/1997', prezzo: '4,30' },
+		{ titolo: 'cvbfgh', autore: 'hgbvm', data: 'bazv', prezzo: 'ryen' },
+		{ titolo: 'Chainsaw Man', autore: 'Tatsuki Fujimoto', data: '"04/03/2019', prezzo: '5,20' }
+	];
+</script>
+
+<h1 class="text-3xl text-center uppercase">Benvenuti!</h1>
+
+<div class="p-10">
+	{#each mangas as manga}
+		<CardManga
+			titolo={manga.titolo}
+			autore={manga.autore}
+			data={manga.data}
+			prezzo={manga.prezzo}
+		/>
+	{/each}
 </div>
+<!--
+Compiti: Aggiungere copertine manga
+         mettere tutto in una griglia
+		 creare una pagina descrizione del manga
+		 -->
