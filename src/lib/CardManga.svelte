73924b4e19641087b46ff8aecd29a42e1f17af02
Laggatraz <brunofederico431@gmail.com>
2024-11-14 15:31:17 +0100

    aggiunto il voto

--- src/lib/CardManga.svelte
+++ src/lib/CardManga.svelte
@@ -3,17 +3,20 @@
 	export var autore: string = 'Eiichiro Oda';
 	export var prezzo: string = '4,30';
 	export var data: string = '22/07/1997';
+	export var immagine: string = 'https://picsum.photos/seed/picsum/200/300';
+	export var voto: string = '10/10';
 </script>
 
 <main class="pb-4">
 	<div class="h-72 w-64 border-2 border-black rounded-lg flex flex-col overflow-hidden">
 		<div class="bg-orange-500 h-[60%] border-b-2 border-black">
-			<img src="81hipaenXVL.jpg" alt="" />
+			<img src={immagine} alt="" />
 		</div>
 		<div class="px-2 pt-1 flex flex-col justify-between h-[40%] bg-white border-t-2 border-black">
 			<div>
 				<p class="text-2xl italic">{titolo}</p>
 				<p>{autore}</p>
+				<p class="flex justify-end">{voto}</p>
 			</div>
 			<div class="flex justify-between">
 				<p>{data}</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,38 @@
 <script lang="ts">
 	import CardManga from '$lib/CardManga.svelte';
 	var mangas = [
-		{ titolo: 'One Piece', autore: 'Eiichiro Oda', data: '22/07/1997', prezzo: '4,30' },
-		{ titolo: 'cvbfgh', autore: 'hgbvm', data: 'bazv', prezzo: 'ryen' },
-		{ titolo: 'Chainsaw Man', autore: 'Tatsuki Fujimoto', data: '04/03/2019', prezzo: '5,20' },
-		{ titolo: 'Chainsaw Man', autore: 'Tatsuki Fujimoto', data: '04/03/2019', prezzo: '5,20' }
+		{
+			titolo: 'One Piece',
+			autore: 'Eiichiro Oda',
+			data: '22/07/1997',
+			prezzo: '4,30',
+			immagine: '81hipaenXVL.jpg',
+			voto: '5/10'
+		},
+		{
+			titolo: 'Jujutsu Kaisen',
+			autore: 'Gege Akutami',
+			data: '04/06/2018',
+			prezzo: '4,50',
+			immagine: 'jjk.jpg',
+			voto: '9/10'
+		},
+		{
+			titolo: 'Chainsaw Man',
+			autore: 'Tatsuki Fujimoto',
+			data: '04/03/2019',
+			prezzo: '5,20',
+			immagine: '71xceC0p1cL._AC_UF1000,1000_QL80_.jpg',
+			voto: '10/10'
+		},
+		{
+			titolo: 'Berserk Collection',
+			autore: 'Kentaro Miura',
+			data: '25/07/1989',
+			prezzo: '5,50',
+			immagine: 'Berserk.jpg',
+			voto: '10/10'
+		}
 	];
 </script>
 
@@ -17,11 +45,14 @@
 			autore={manga.autore}
 			data={manga.data}
 			prezzo={manga.prezzo}
+			immagine={manga.immagine}
+			voto={manga.voto}
 		/>
 	{/each}
 </div>
 <!--
-Compiti: Aggiungere copertine manga
+Compiti: V Aggiungere copertine manga
          mettere tutto in una griglia
 		 creare una pagina descrizione del manga
+		 V aggiungere una variabile ranking
 		 -->
